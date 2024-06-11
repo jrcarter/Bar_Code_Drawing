e8c96c2d86f71c42a47371fd7de8ab8a20e02843
Jeffrey R. Carter <16976909+jrcarter@users.noreply.github.com>
2024-06-11 15:38:43 +0200

    Select QR ECL

--- svgqr.adb
+++ svgqr.adb
@@ -10,24 +10,43 @@ with Bar_Code_Drawing.How.SVG;
 with Bar_Code_Drawing.What.QR_Code;
 
 procedure SVGQR is
-   -- Empty
+   package CQR renames Bar_Code_Drawing.What.QR_Code;
+
+   Text_Arg : Positive := 1;
+   ECL      : CQR.Error_Correction_Level := CQR.Medium;
 begin -- SVGQR
    if Ada.Command_Line.Argument_Count < 1 then
-      Ada.Text_IO.Put_Line (Item => "Usage: pbmqr <text to encode>");
+      Ada.Text_IO.Put_Line (Item => "Usage: svgqr [-L | -M | -Q | -H] <text to encode>");
+      Ada.Text_IO.Put_Line (Item => "   Switch indicates the Error Correction Level to use (default M)");
       Ada.Text_IO.Put_Line (Item => "   Outputs a QR code containing the argument to standard output");
 
       return;
    end if;
 
-   Get_Text : declare
-      package CQR renames Bar_Code_Drawing.What.QR_Code;
+   if Ada.Command_Line.Argument_Count > 1 then
+      Check_Switch : declare
+         Arg : constant String := Ada.Command_Line.Argument (1);
+      begin -- Check_Switch
+         if Arg'Length = 2 and then (Arg (1) = '-' and Arg (2) in 'l' | 'L' | 'm' | 'M' | 'q' | 'Q' | 'h' | 'H') then
+            Text_Arg := 2;
 
-      Text  : constant String   := Ada.Command_Line.Argument (1);
-      Width : constant Natural  := CQR.Width (Text);
+            ECL := (case Arg (2) is
+                    when 'l' | 'L' => CQR.Low,
+                    when 'm' | 'M' => CQR.Medium,
+                    when 'q' | 'Q' => CQR.Quartile,
+                    when 'h' | 'H' => CQR.High,
+                    when others    => raise Program_Error with "Invalid level");
+         end if;
+      end Check_Switch;
+   end if;
+
+   Get_Text : declare
+      Text  : constant String   := Ada.Command_Line.Argument (Text_Arg);
+      Width : constant Natural  := CQR.Width (Text, ECL);
 
       Info : Bar_Code_Drawing.Drawing_Info := Bar_Code_Drawing.New_Info (Width, Width, 2);
    begin -- Get_Text
-      CQR.Draw (Info => Info, Text => Text);
+      CQR.Draw (Info => Info, Text => Text, Level => ECL);
       Ada.Text_IO.Put (Item => Bar_Code_Drawing.How.SVG.Image (Info, 1.0) );
    exception -- Get_Text
    when others =>
